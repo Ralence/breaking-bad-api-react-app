62d535f748b0a07e7c7596a67d4f16c25af6351b
Ralence <ranko.ostojic@gmail.com>
2020-07-07 21:09:00 +0200

    feat: add styles to app.scc and header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Header from "./components/UI/Header";
+
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+};
 
 export default App;
