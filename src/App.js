238998371fbdf468a3c6bd76afe1eb5c2a9ea411
Ralence <ranko.ostojic@gmail.com>
2020-07-07 21:43:34 +0200

    feat: add character gid and character item grid  card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/UI/Header";
+import CharacterGrid from "./components/characters/CharacterGrid";
+
+import axios from "axios";
 
 import "./App.css";
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const res = await axios(`https://www.breakingbadapi.com/api/characters`);
+
+      console.log(res.data);
+
+      setItems(res.data);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, []);
+
   return (
     <div className="container">
       <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 };
